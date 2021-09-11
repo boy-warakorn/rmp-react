043481bc250ae41375b1ef413341c57888dd576e
Warakorn <boydoubleb@gmail.com>
2021-09-11 18:33:42 +0700

    chore: comment font-size for more clarifying

--- src/components/typography/Typography.tsx
+++ src/components/typography/Typography.tsx
@@ -1,26 +1,32 @@
 import styled from "@emotion/styled";
 import tw from "twin.macro";
 
+// font-size: 36px
 export const HeadingText1 = styled.div`
   ${tw`text-4xl`}
 `;
 
+// font-size: 24px
 export const HeadingText2 = styled.div`
   ${tw`text-2xl`}
 `;
 
+// font-size: 20px
 export const HeadingText3 = styled.div`
   ${tw`text-xl`}
 `;
 
+// font-size: 16px
 export const HeadingText4 = styled.div`
   ${tw`text-base`}
 `;
 
+// font-size: 14px
 export const SubHeadingText1 = styled.div`
   ${tw`text-sm`}
 `;
 
+// font-size: 22px
 export const SubtitleText1 = styled.div`
   font-size: 1.38 rem;
   ${tw`text-grey`}

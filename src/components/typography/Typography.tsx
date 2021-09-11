1da10cf1c359b6850914147b9d5a463f28e19312
Warakorn <boydoubleb@gmail.com>
2021-09-11 14:38:30 +0700

    add: general typo

---
+++ src/components/typography/Typography.tsx
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+import tw from "twin.macro";
+
+export const HeadingText1 = styled.div`
+  ${tw`text-4xl`}
+`;
+
+export const HeadingText2 = styled.div`
+  ${tw`text-xl`}
+`;
+
+export const SubtitleText1 = styled.div`
+  font-size: 1.38 rem;
+  ${tw`text-grey`}
+`;

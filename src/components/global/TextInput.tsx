b78a45cee3df635ae5e096a349d830921c0510ee
Warakorn <boydoubleb@gmail.com>
2021-09-11 14:38:21 +0700

    add: custom input

---
+++ src/components/global/TextInput.tsx
@@ -0,0 +1,38 @@
+import React, { CSSProperties } from "react";
+import { Input } from "antd";
+import styled from "@emotion/styled";
+
+export interface InputProps {
+  allowClear?: boolean;
+  placeholder?: string;
+  size?: "large" | "middle" | "small";
+  maxLength?: number;
+  prefix?: React.ReactNode;
+  suffix?: React.ReactNode;
+  type?: string;
+  value?: string | number;
+  id?: string;
+  disabled?: boolean;
+  defaultValue?: string | number;
+  state?: string;
+  width?: string;
+  onChange?: any;
+  style?: CSSProperties;
+  onPressEnter?: any;
+  ref?: any;
+  className?: string;
+}
+
+const LoginInput = styled(Input)<InputProps>`
+  width: 55%;
+  & .ant-input-lg {
+    font-weight: 400;
+    font-size: 14px;
+  }
+`;
+
+const TextInput = (props: InputProps) => {
+  return <LoginInput {...props} />;
+};
+
+export default TextInput;

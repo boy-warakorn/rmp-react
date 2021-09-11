3f3f8556faa13da91c383ed709df1e7a36e9474a
Warakorn <boydoubleb@gmail.com>
2021-09-11 18:11:31 +0700

    adjust: sider and input width

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,8 +3,9 @@ import React, { Fragment } from "react";
 const HomePage = () => {
   return (
     <Fragment>
-      <div className="col-span-12">Text Text Text Text Text Text</div>
-      <div className="col-span-12">Text Text Text Text Text Text</div>
+      <div className="col-span-12" style={{ height: "2000px" }}>
+        Text Text Text Text Text Text
+      </div>
     </Fragment>
   );
 };

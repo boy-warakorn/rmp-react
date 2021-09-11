5b9364654f513c567d5d3ea46e87ec4962c2fb60
Warakorn <boydoubleb@gmail.com>
2021-09-12 01:23:10 +0700

    add: netlify config

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,7 +2,6 @@ import Layout from "@components/global/layout/Layout";
 import React from "react";
 import { Route, Switch } from "react-router";
 import LoginPage from "./auth/LoginPage";
-import "antd/dist/antd.css";
 
 function App() {
   return (

3413092dd36db8cfeb9ec986556bcf8d7d60e3a4
Warakorn <boydoubleb@gmail.com>
2021-09-11 15:45:59 +0700

    update: typography

--- src/components/typography/Typography.tsx
+++ src/components/typography/Typography.tsx
@@ -9,6 +9,10 @@ export const HeadingText2 = styled.div`
   ${tw`text-xl`}
 `;
 
+export const HeadingText3 = styled.div`
+  ${tw`text-base`}
+`;
+
 export const SubtitleText1 = styled.div`
   font-size: 1.38 rem;
   ${tw`text-grey`}

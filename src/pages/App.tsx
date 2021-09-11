4f9921e85c40e8f349743e265a4ccefebc08f36d
Warakorn <boydoubleb@gmail.com>
2021-09-12 01:14:35 +0700

    update: antd script

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,6 +2,7 @@ import Layout from "@components/global/layout/Layout";
 import React from "react";
 import { Route, Switch } from "react-router";
 import LoginPage from "./auth/LoginPage";
+import "antd/dist/antd.css";
 
 function App() {
   return (

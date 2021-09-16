900fe362f6cca4f4326cdf7a580b53ea71fa14a3
Warakorn <boydoubleb@gmail.com>
2021-09-16 10:07:36 +0700

    add: form functionality

--- src/pages/rooms/crud/AddOwnerPage.tsx
+++ src/pages/rooms/crud/AddOwnerPage.tsx
@@ -1,6 +1,7 @@
 import Card, { FormCard } from "@components/global/Card";
 import TextInput from "@components/global/form/TextInput";
 import Button from "@components/global/Button";
+import { Form } from "antd";
 import {
   BodyText1,
   HeadingText3,
@@ -15,6 +16,7 @@ const { RangePicker } = DatePicker;
 const AddOwnerPage = () => {
   const { id } = useParams<{ id: string }>();
   const [isEdit, setIsEdit] = useState(false);
+  const [form] = Form.useForm();
 
   const path = window.location.pathname.split("/")[4];
 
@@ -22,6 +24,10 @@ const AddOwnerPage = () => {
     if (path === "edit") setIsEdit(true);
     // eslint-disable-next-line
   }, []);
+
+  const onFinish = () => {
+    console.log("form.getFieldValues :>> ", form.getFieldsValue());
+  };
 
   return (
     <Fragment>
@@ -32,32 +38,50 @@ const AddOwnerPage = () => {
       </div>
       <Card className="p-9 col-span-12">
         <HeadingText4>General Information</HeadingText4>
-        <FormCard className="grid grid-cols-6 gap-x-4 mt-4">
-          <div className="col-span-2">
-            <TextInput title="Name" />
+        <Form form={form} onFinish={onFinish}>
+          <FormCard className="grid grid-cols-6 gap-x-4 mt-4">
+            <Form.Item
+              name={["name"]}
+              rules={[{ required: true }]}
+              className="col-span-2"
+            >
+              <TextInput title="Name" />
+            </Form.Item>
+            <Form.Item
+              name={["phoneNumber"]}
+              rules={[{ pattern: RegExp("^[0][0-9]{9}$") }]}
+              className="col-span-1"
+            >
+              <TextInput title="Phone" />
+            </Form.Item>
+            <div className="col-span-3"></div>
+            <Form.Item
+              name={["citizenNumber"]}
+              rules={[{ pattern: RegExp("^[0-9]{13}$") }]}
+              className="col-span-2 mt-6"
+            >
+              <TextInput title="Citizen ID Number" />
+            </Form.Item>
+          </FormCard>
+          <HeadingText4 className="mt-9">Contract Information</HeadingText4>
+          <FormCard className="grid grid-cols-6 gap-x-4 mt-4">
+            <div className="col-span-2 flex flex-col">
+              <BodyText1 className="font-bold mb-2">
+                Date move in {">"} Date move out
+              </BodyText1>
+              <RangePicker />
+            </div>
+          </FormCard>
+          <div className="flex justify-end mt-9">
+            <Button
+              color="primary"
+              className="px-12 font-roboto text-sm"
+              htmlType="submit"
+            >
+              Save
+            </Button>
           </div>
-          <div className="col-span-1">
-            <TextInput title="Phone" />
-          </div>
-          <div className="col-span-3"></div>
-          <div className="col-span-2 mt-6">
-            <TextInput title="Citizen ID Number" />
-          </div>
-        </FormCard>
-        <HeadingText4 className="mt-9">Contract Information</HeadingText4>
-        <FormCard className="grid grid-cols-6 gap-x-4 mt-4">
-          <div className="col-span-2 flex flex-col">
-            <BodyText1 className="font-bold mb-2">
-              Date move in {">"} Date move out
-            </BodyText1>
-            <RangePicker />
-          </div>
-        </FormCard>
-        <div className="flex justify-end mt-9">
-          <Button color="primary" className="px-12 font-roboto text-sm">
-            Save
-          </Button>
-        </div>
+        </Form>
       </Card>
     </Fragment>
   );

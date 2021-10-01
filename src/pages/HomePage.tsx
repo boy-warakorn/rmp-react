85f05526fb259e37c1069ab03dd29b1da163381e
Warakorn <boydoubleb@gmail.com>
2021-10-01 12:55:57 +0700

    change: enable all feats for admin

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,6 @@ import {
   HeadingText4,
   SubHeadingText1,
 } from "@components/global/typography/Typography";
-// import CustomSelect from "@components/global/form/Select";
 import React, { Fragment } from "react";
 import { SlidersFilled } from "@ant-design/icons";
 import DashboardCard from "@components/feature/dashboard/DashboardCard";
@@ -16,8 +15,6 @@ import { useHistory } from "react-router";
 import { useSelector } from "react-redux";
 import { userSelector } from "@stores/user/selector";
 
-// const { Option } = Select;
-
 const HomePage = () => {
   const history = useHistory();
   const user = useSelector(userSelector);
@@ -26,10 +23,6 @@ const HomePage = () => {
     <Fragment>
       <div className="col-span-12 flex justify-between items-center mb-6">
         <HeadingText4>Summary</HeadingText4>
-        {/* <CustomSelect defaultValue="This month" placeholder="Select period">
-          <Option value="This month">This month</Option>
-          <Option value="This year">This year</Option>
-        </CustomSelect> */}
       </div>
       <div className="col-span-6 md:col-span-3">
         <DashboardCard text="Payment Dues" count={60} />
@@ -48,37 +41,32 @@ const HomePage = () => {
           {user.role === "admin" ? "Room summary" : "Recent incoming packages"}
         </HeadingText4>
       </div>
-      {user.role !== "admin" && (
-        <div className="col-span-6">
-          <Fragment>
-            <DashboardPackageCard />
-            <div className="mb-4"></div>
-            <DashboardPackageCard />
-            <div className="mb-4"></div>
-            <DashboardPackageCard />
-            <div className="flex items-center justify-center">
-              <Button
-                color="primary"
-                className="px-9 mt-4 center rounded"
-                onClick={() => history.push("/packages")}
-              >
-                <SubHeadingText1 className="font-roboto">
-                  More Detail
-                </SubHeadingText1>
-              </Button>
-            </div>
-          </Fragment>
-        </div>
-      )}
+
+      <div className="col-span-6">
+        <Fragment>
+          <DashboardPackageCard />
+          <div className="mb-4"></div>
+          <DashboardPackageCard />
+          <div className="mb-4"></div>
+          <DashboardPackageCard />
+          <div className="flex items-center justify-center">
+            <Button
+              color="primary"
+              className="px-9 mt-4 center rounded"
+              onClick={() => history.replace("/packages")}
+            >
+              <SubHeadingText1 className="font-roboto">
+                More Detail
+              </SubHeadingText1>
+            </Button>
+          </div>
+        </Fragment>
+      </div>
 
       <div className="col-span-6">
         <Card className="p-4 ">
           <div className="flex justify-between">
             <HeadingText4>Room occupied</HeadingText4>
-            {/* <SettingFilled
-              style={{ fontSize: "32px", cursor: "pointer" }}
-              className="icon-spin"
-            /> */}
           </div>
           <div className="flex items-center">
             <SlidersFilled style={{ fontSize: "18px" }} />
@@ -87,29 +75,26 @@ const HomePage = () => {
           <RoomOccupiedChart />
         </Card>
       </div>
-      {user.role !== "admin" && (
-        <Fragment>
-          <div className="col-span-12 mt-9">
-            <HeadingText4>Latest reports</HeadingText4>
-          </div>
-          <div className="col-span-12 mt-6">
-            <DashboardReportCard />
-            <div className="mt-4"></div>
-            <DashboardReportCard />
-            <div className="flex items-center justify-center">
-              <Button
-                color="primary"
-                className="px-9 mt-4 center rounded"
-                onClick={() => history.push("/reports")}
-              >
-                <SubHeadingText1 className="font-roboto">
-                  More Detail
-                </SubHeadingText1>
-              </Button>
-            </div>
-          </div>
-        </Fragment>
-      )}
+
+      <div className="col-span-12 mt-9">
+        <HeadingText4>Latest reports</HeadingText4>
+      </div>
+      <div className="col-span-12 mt-6">
+        <DashboardReportCard />
+        <div className="mt-4"></div>
+        <DashboardReportCard />
+        <div className="flex items-center justify-center">
+          <Button
+            color="primary"
+            className="px-9 mt-4 center rounded"
+            onClick={() => history.replace("/reports")}
+          >
+            <SubHeadingText1 className="font-roboto">
+              More Detail
+            </SubHeadingText1>
+          </Button>
+        </div>
+      </div>
     </Fragment>
   );
 };
